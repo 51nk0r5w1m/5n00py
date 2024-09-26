0a23a95d940e15768f0ed2326ff0c827b4087195
51nk0r5w1m <111602071+51nk0r5w1m@users.noreply.github.com>
2024-09-26 11:24:27 -0400

    Update api.ts
    quotable.io api down - replaced with https://zenquotes.io/api/random

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -23,7 +23,7 @@ export const getWeather = async (city: string) => {
 };
 
 export const getQuote = async () => {
-  const { data } = await axios.get('http://api.quotable.io/random');
+  const { data } = await axios.get('https://zenquotes.io/api/random');
   return {
     quote: `“${data.content}” — ${data.author}`,
   };
